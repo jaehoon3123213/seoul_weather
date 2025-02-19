836767ce7f30dd000ce8c6a2f64fe9d2b982c762
jaehoon3123213 <boxstar120@gmail.com>
2025-02-19 14:33:08 +0900

    Create weather_script.py

---
+++ weather_script.py
@@ -0,0 +1,29 @@
+import requests
+import csv
+from datetime import datetime
+import os
+import pandas as pd
+CITY ="Seoul"
+API_KEY="os.getenv("OPENWEATHER_API_KEY")
+url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+data =requests.get(url).json()
+
+temp = data["main"]["temp"]
+humidity =data["main"]["humidity"]
+weather = data["weather"][0]["description"]
+time_zone = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+csv_filename = "seoul_weather.csv"
+header =["timezone","temp","humidity","weather"]
+
+file_exist = os.path.isfile(csv_filename)
+
+#mode a - if not file - create
+#if is file - write
+with open(csv_filename,"a") as file:
+    writer = csv.writer(file)
+    if not file_exist:
+         writer.writerow(header)
+    writer.writerow([time_zone,temp,humidity,weather])
+    print("csv저장완료")
